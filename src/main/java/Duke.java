476e37dad5a5a7db236e56c2f8fff8dc31a9653d
shiorin9 <homingjun9@gmail.com>
2020-08-22 01:12:22 +0800

    Level 3 & A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,46 +1,46 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         String line = "―――――――――――――――――――――――――――――――――";
+        Task[] tasks = new Task[100];
+        Scanner scan = new Scanner(System.in);
+        String scannedInput;
+        int numberOfTasks = 0;
+
         System.out.println(line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println();
-        Scanner sc = new Scanner(System.in);
-        String ls = sc.nextLine();
-        //System.out.println(ls.equals("list"));
-        String[] list = new String[100];
-        for (int i = 0; i < 100; i++) {
-            if (ls.equals("list") == false) {
-                list[i] = ls;
-                System.out.println(line);
-                System.out.println("added: " + list[i]);
-                System.out.println(line);
-                System.out.println();
-            } else {
-                break;
-            }
-            ls = sc.nextLine();
-        }
-        System.out.println(line);
-        for (int i = 0; i < 100; i++) {
-            if (list[i] != null) {
-                System.out.println((i + 1) + "." + list[i]);
-            }
-        }
+        System.out.println(" Hi I'm Savage bot! I'm here to make ur day worse :)");
+        System.out.println("             What u wan me do for u sia?");
         System.out.println(line);
         System.out.println();
-        String bye = sc.nextLine();
+
+        while (!(scannedInput = scan.nextLine()).contains("bye")) {
+            System.out.println(line);
+            if (scannedInput.equals("list")) {
+                if (numberOfTasks == 0) {
+                    System.out.println("Eh your list still empty la can wake up and use ur brains abit anot?");
+                    System.out.println(line);
+                } else {
+                    System.out.println("Although I dun think u can finish any of them, here is ur dumb list:");
+                }
+                for (int i = 0; i < numberOfTasks; i++) {
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
+                }
+            } else if (!scannedInput.contains("done")) {
+                tasks[numberOfTasks] = new Task(scannedInput);
+                numberOfTasks++;
+                System.out.println("added:" + scannedInput);
+                }
+            if (scannedInput.contains("done")) {
+                String[] split = scannedInput.split(" ");
+                tasks[Integer.parseInt(split[1]) - 1].markAsDone();
+                System.out.println("Wow u actually managed to complete " + tasks[Integer.parseInt(split[1]) - 1].description + ". I'm surprised u can even complete a task, I guess I'll give props to u.");
+                System.out.println("[" + tasks[Integer.parseInt(split[1]) - 1].getStatusIcon() + "] " + tasks[Integer.parseInt(split[1]) - 1].description);
+            }
+            System.out.println();
+            }
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println("Until next time, dun die pls. Ciao.");
     }
 }
+
