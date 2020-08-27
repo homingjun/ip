cb3b930cafc5454912c79acc01fea7fff20372bd
shiorin9 <homingjun9@gmail.com>
2020-08-27 18:12:40 +0800

    Revamp code arrangement to improve clarity and follow A-Coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,46 +1,74 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printLine(String text) {
+        String line = "―――――――――――――――――――――――――――――――――\n";
+        System.out.println(line + text + line);
+    }
+
+    public static String greetings() {
+        return " Hi I'm Savage bot! I'm here to make ur day worse :)\n"
+                + "             What u wan me do for u sia?\n";
+    }
+
+    public static String printListDescription(Task[] tasks, int numberOfTasks, int completedTasks) {
+        if (numberOfTasks == 0) {
+            return "Eh your list still empty la can wake up and use ur brains abit anot?\n";
+        } else {
+            for (int taskNumber = 0; taskNumber < numberOfTasks; taskNumber++) {
+                if (tasks[taskNumber].isDone && completedTasks == numberOfTasks) {
+                    return "Wow, u actually finished ur tasks! I guess miracles do happen after all.\n";
+                } else if (completedTasks == 0) {
+                    return "Although I dun think u can finish any of them, here is ur dumb list:\n";
+                } else {
+                    return "As expected, here is ur incomplete task list :>\n";
+                }
+            }
+        }
+        return "";
+    }
+
+    public static String printListItems(Task[] tasks, int numberOfTasks) {
+        String listItems = "";
+        for (int i = 0; i < numberOfTasks; i++) {
+            listItems += (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description + "\n";
+        }
+        return listItems;
+    }
+
+    public static String printTasks(Task[] tasks, String scannedInput, int numberOfTasks) {
+        tasks[numberOfTasks] = new Task(scannedInput);
+        return "added:" + scannedInput + "\n";
+    }
+
+    public static String completeTask(Task[] tasks, String scannedInput) {
+        String[] split = scannedInput.split(" ");
+        tasks[Integer.parseInt(split[1]) - 1].markAsDone();
+        return "Wow... I guess u're not retarded after all! Congrats on completing this task :>\n"
+                + "[" + tasks[Integer.parseInt(split[1]) - 1].getStatusIcon() + "] "
+                + tasks[Integer.parseInt(split[1]) - 1].description + "\n";
+    }
+
     public static void main(String[] args) {
-        String line = "―――――――――――――――――――――――――――――――――";
         Task[] tasks = new Task[100];
         Scanner scan = new Scanner(System.in);
         String scannedInput;
         int numberOfTasks = 0;
+        int completedTasks = 0;
 
-        System.out.println(line);
-        System.out.println(" Hi I'm Savage bot! I'm here to make ur day worse :)");
-        System.out.println("             What u wan me do for u sia?");
-        System.out.println(line);
-        System.out.println();
-
+        printLine(greetings());
         while (!(scannedInput = scan.nextLine()).contains("bye")) {
-            System.out.println(line);
             if (scannedInput.equals("list")) {
-                if (numberOfTasks == 0) {
-                    System.out.println("Eh your list still empty la can wake up and use ur brains abit anot?");
-                    System.out.println(line);
-                } else {
-                    System.out.println("Although I dun think u can finish any of them, here is ur dumb list:");
-                }
-                for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
-                }
+                printLine(printListDescription(tasks, numberOfTasks, completedTasks) + printListItems(tasks, numberOfTasks));
             } else if (!scannedInput.contains("done")) {
-                tasks[numberOfTasks] = new Task(scannedInput);
+                printLine(printTasks(tasks, scannedInput, numberOfTasks));
                 numberOfTasks++;
-                System.out.println("added:" + scannedInput);
-                }
+            }
             if (scannedInput.contains("done")) {
-                String[] split = scannedInput.split(" ");
-                tasks[Integer.parseInt(split[1]) - 1].markAsDone();
-                System.out.println("Wow u actually managed to complete " + tasks[Integer.parseInt(split[1]) - 1].description + ". I'm surprised u can even complete a task, I guess I'll give props to u.");
-                System.out.println("[" + tasks[Integer.parseInt(split[1]) - 1].getStatusIcon() + "] " + tasks[Integer.parseInt(split[1]) - 1].description);
+                printLine(completeTask(tasks, scannedInput));
+                completedTasks++;
             }
-            System.out.println();
-            }
-        System.out.println(line);
-        System.out.println("Until next time, dun die pls. Ciao.");
+        }
+        printLine("Until next time, dun die pls. Ciao.\n");
     }
 }
-
